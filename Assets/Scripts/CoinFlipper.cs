8aa069e983f9a7d1092c2a675b5961c42276b14b
Elif Nazlı Böke <111501335+elifnazlib@users.noreply.github.com>
2024-08-20 16:28:34 +0300

    Cheat Panel Added

--- Assets/Scripts/CoinFlipper.cs
+++ Assets/Scripts/CoinFlipper.cs
@@ -1,34 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class CoinFlipper : MonoBehaviour
 {
     [SerializeField] private GameObject yazi;
     [SerializeField] private GameObject tura;
+    [SerializeField] private GameObject cheatPanel;
+    [SerializeField] private Slider cheatSlider;
     private Random rnd;
+    private int pressCount = 0;
+    [SerializeField] private int yaziOrani = 50;
     void Start()
     {
         rnd = new Random();
     }
 
     public void Flip() {
-        int num  = rnd.Next(1, 3);  // creates a number between 1 and 2
+        int num  = rnd.Next(1, 101);  // creates a number between 1 and 2
 
-        switch (num)
-        {
-            case 1:
-                Debug.Log("yazi");
-                yazi.SetActive(true);
-                tura.SetActive(false);
-                break;
-            case 2:
-                Debug.Log("tura");
-                tura.SetActive(true);
-                yazi.SetActive(false);
-                break;
+        if (num < yaziOrani) {
+            Debug.Log("yazi");
+            yazi.SetActive(true);
+            tura.SetActive(false);
+        } else {
+            Debug.Log("tura");
+            tura.SetActive(true);
+            yazi.SetActive(false);
         }
+    }
+
+    public void CheatFlip() {
+        pressCount++;
+        Debug.Log("cheat button pressed");
+        if (pressCount == 7) {
+            pressCount = 0;
+            cheatPanel.SetActive(true);
+        }
+    }
+
+    public void ExitCheatPanel() {
+        cheatPanel.SetActive(false);
+    }
+
+    public void CheatSliderChange() {
+        yaziOrani = (int) cheatSlider.value;
     }
 
 }
